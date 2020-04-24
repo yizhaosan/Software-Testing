2996e46b7a971fd48dfff4c3252b0496e82396d2
yizhaosan <2316453754@qq.com>
2020-04-24 11:28:07 +0800

    修改窗口标题

--- src/main/java/nl/tudelft/jpacman/ui/PacManUI.java
+++ src/main/java/nl/tudelft/jpacman/ui/PacManUI.java
@@ -68,7 +68,8 @@ public class PacManUI extends JFrame {
     public PacManUI(final Game game, final Map<String, Action> buttons,
                     final Map<Integer, Action> keyMappings,
                     ScoreFormatter scoreFormatter) {
-        super("JPacman");
+//        super("JPacman");
+    	super("JPacman 2020");
         assert game != null;
         assert buttons != null;
         assert keyMappings != null;
