f3826d007a80138ae5a6f1afa82fd8a8a5e4df73
yizhaosan <2316453754@qq.com>
2020-05-03 17:24:30 +0800

    实验十一，测试board.Board.withinBoard方法，边界值测试。初始化

---
+++ experiment11_withinBorders/src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
@@ -0,0 +1,70 @@
+package nl.tudelft.jpacman.board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test suite to confirm that {@link Unit}s correctly (de)occupy squares.
+ *
+ * @author Jeroen Roosen 
+ *
+ */
+class OccupantTest {
+
+    /**
+     * The unit under test.
+     */
+    private Unit unit;
+
+    /**
+     * Resets the unit under test.
+     */
+    @BeforeEach
+    void setUp() {
+        unit = new BasicUnit();
+    }
+
+    /**
+     * Asserts that a unit has no square to start with.
+     */
+    @Test
+    void noStartSquare() {
+        // Remove the following placeholder:
+        assertThat(unit).isNotNull();
+        assertThat(unit.hasSquare()).isFalse();
+    }
+
+    /**
+     * Tests that the unit indeed has the target square as its base after
+     * occupation.
+     */
+    @Test
+    void testOccupy() {
+        // Remove the following placeholder:
+        assertThat(unit).isNotNull();
+        Square square = new BasicSquare();
+        unit.occupy(square);
+
+        assertThat(unit.getSquare()).isEqualTo(square);
+        assertThat(square.getOccupants()).contains(unit);
+    }
+
+    /**
+     * Test that the unit indeed has the target square as its base after
+     * double occupation.
+     */
+    @Test
+    void testReoccupy() {
+        // Remove the following placeholder:
+        assertThat(unit).isNotNull();
+        Square square = new BasicSquare();
+
+        unit.occupy(square);
+        unit.occupy(square);
+
+        assertThat(unit.getSquare()).isEqualTo(square);
+        assertThat(square.getOccupants()).contains(unit).containsOnlyOnce(unit);
+    }
+}
