c85f5a79278d00c9dda1e381e3bcb306ece9e1a4
yizhaosan <2316453754@qq.com>
2020-05-28 17:21:09 +0800

    期中考试模拟题，第一题、第二题。修正

--- experiment12_level.MapParser/src/test/java/nl/tudelft/jpacman/game/GameStartTest.java
+++ experiment12_level.MapParser/src/test/java/nl/tudelft/jpacman/game/GameStartTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -87,6 +85,7 @@ public class GameStartTest {
         when(level.remainingPellets()).thenReturn(23);
 
         game = new SinglePlayerGame(mock(Player.class), level, mock(PointCalculator.class));
+        assertThat(game.isInProgress()).isFalse();
 
         game.start();
         assertThat(game.isInProgress()).isTrue();
@@ -97,86 +96,5 @@ public class GameStartTest {
         verify(level, times(1)).isAnyPlayerAlive();
         verify(level, times(1)).remainingPellets();
     }
-
-/*
-    Game game = mock(Game.class);
-    Level level = mock(Level.class);
-
-    @Test
-    @DisplayName("Game Stop")
-    @Order(1)
-    void testIsInProgress() {
-        game.isInProgress();
-        assertThat(game.isInProgress()).isFalse();
-    }
-
-    @Test
-    @DisplayName("Player存活与豆子数量test")
-    @Order(2)
-    @ParameterizedTest
-    @CsvSource({
-        "true, 1, false",
-        "true, 0, false",
-        "fasle, 1, fasle",
-        "fasle, 0, fasle"
-    })
-    void testGameStart(boolean x, int y, boolean result) {
-        boolean flag;
-        x = level.isAnyPlayerAlive();
-        y = level.remainingPellets();
-        if (!x && (y > 0)) {
-            flag = true;
-        } else {
-            flag = false;
-        }
-        assertThat(flag).isEqualTo(result);
-    }
-*/
-
-/*
-    PointCalculator pointCalculator = mock(PointCalculator.class);
-    Level level = mock(Level.class);
-
-    private final Game game = new Game(pointCalculator) {
-        @Override
-        public List<Player> getPlayers() {
-            return null;
-        }
-
-        @Override
-        public Level getLevel() {
-            return null;
-        }
-    };
-
-    @Test
-    void testIsInProgess() {
-        assertThat(game.isInProgress()).isEqualTo(false);
-    }
-
-    @Test
-    void testIsAnyPlayerAlive() {
-        assertThat(level.isAnyPlayerAlive()).isEqualTo(true);
-    }
-
-    @Test
-    void testRemainingPellets() {
-        assertThat(level.remainingPellets()).isGreaterThan(0);
-    }
-    @Test
-    @DisplayName("Game.start方法测试")
-    @CsvSource({
-        "0, 0, 0, false",
-        "0, 0, 1, false",
-        "0, 1, 0, false",
-        "0, 1, 1, true",
-        "1, 0, 0, false",
-        "1, 0, 1, false",
-        "1, 1, 1, false",
-    })
-    void testGameStart(boolean x, boolean y, int z, boolean result) {
-        assertThat(game.isInProgress()).isEqualTo(true);
-    }
-*/
 
 }
